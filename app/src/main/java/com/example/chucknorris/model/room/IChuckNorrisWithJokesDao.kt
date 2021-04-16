c74980e75ce882fdaec83c2f16994d37c923dcb2
Cameron <cameron.fisher@absa.africa>
2021-04-16 12:41:34 +0200

    Annotation IChuckNorrisWithJokesDao with Dao.
    - IChuckNorrisWithJokesDao.kt: Added annotation.

--- app/src/main/java/com/example/chucknorris/model/room/IChuckNorrisWithJokesDao.kt
+++ app/src/main/java/com/example/chucknorris/model/room/IChuckNorrisWithJokesDao.kt
@@ -1,6 +1,8 @@
 package com.example.chucknorris.model.room
 
+import androidx.room.Dao
 import com.example.chucknorris.model.models.ChuckNorrisWithJokes
 
+@Dao
 interface IChuckNorrisWithJokesDao: IBaseDao<ChuckNorrisWithJokes> {
 }
