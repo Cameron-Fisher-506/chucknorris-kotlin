c7fe442d3aff7f4b161f65057654d8d7d34db2db
Cameron <cameron.fisher@absa.africa>
2021-04-16 10:04:23 +0200

    Made the get method inline and reified
    - WSCallsUtils.kt: Made the get method reified.

--- app/src/main/java/com/example/chucknorris/utils/WSCallsUtils.kt
+++ app/src/main/java/com/example/chucknorris/utils/WSCallsUtils.kt
@@ -1,24 +1,37 @@
 package com.example.chucknorris.utils
 
 import androidx.lifecycle.liveData
+import androidx.sqlite.db.SimpleSQLiteQuery
 import com.example.chucknorris.enum.Status
+import com.example.chucknorris.model.models.Joke
 import com.example.chucknorris.model.models.Jokes
+import com.example.chucknorris.model.room.ChuckNorrisDatabase
 import com.example.chucknorris.model.room.IJokeDao
 import kotlinx.coroutines.Dispatchers
 
 object WSCallsUtils {
-    fun <T> get(daoJokeDao: IJokeDao, wsCall: suspend () -> Resource<T>) =
+    inline fun <reified T> get(jokeDao: IJokeDao, crossinline wsCall: suspend () -> Resource<T>) =
         liveData<Resource<T>>(Dispatchers.IO) {
             emit(Resource.loading())
+
             val response = wsCall.invoke()
             if (response.status == Status.SUCCESS) {
                 emit(Resource.success(response.data))
                 //cache
                 if (response.data is Jokes) {
-                    daoJokeDao.insert(response.data.jokes)
+                    jokeDao.insert(response.data.jokes)
                 }
             } else {
-                emit(Resource.error(response.message ?: "Service failed: Please contact developer!"))
+                val result = jokeDao.specialQuery(SimpleSQLiteQuery("SELECT * FROM ${Joke::class.java.simpleName}"))
+                if (result != null) {
+                    val jokes = Jokes().apply {
+                        jokes = result
+                    }
+                    emit(Resource.success(jokes as T))
+                } else {
+                    // No data to display
+                    emit(Resource.error("No Connection."))
+                }
             }
         }
 }
