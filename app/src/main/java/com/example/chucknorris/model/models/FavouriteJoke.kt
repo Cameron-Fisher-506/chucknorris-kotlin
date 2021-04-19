955525d8d0a4c37218fbda6b09992a929c68238e
Cameron <cameron.fisher@absa.africa>
2021-04-19 15:33:06 +0200

    Added formatting.
    - FavouriteJoke.kt: Added formatting.

--- app/src/main/java/com/example/chucknorris/model/models/FavouriteJoke.kt
+++ app/src/main/java/com/example/chucknorris/model/models/FavouriteJoke.kt
@@ -1,16 +1,17 @@
 package com.example.chucknorris.model.models
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
 @Entity
 data class FavouriteJoke(
-                    var createdAt: String = "",
-                    var iconUrl: String = "",
-                    var updatedAt: String = "",
-                    var url: String = "",
-                    var value: String = ""
-): BaseModel(), Serializable {
+    var createdAt: String = "",
+    var iconUrl: String = "",
+    var updatedAt: String = "",
+    var url: String = "",
+    var value: String = ""
+) : BaseModel(), Serializable {
     @PrimaryKey(autoGenerate = true)
     override var id: Int = 0
 }
