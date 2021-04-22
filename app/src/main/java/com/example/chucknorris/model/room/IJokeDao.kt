55325d5092fb045aa9bd7223cb4a1de16d5326d3
Cameron <cameron.fisher@absa.africa>
2021-04-22 12:49:50 +0200

    Updated the sql select query.
    - IJokeDao.kt: Updated the query to use like with % operator

--- app/src/main/java/com/example/chucknorris/model/room/IJokeDao.kt
+++ app/src/main/java/com/example/chucknorris/model/room/IJokeDao.kt
@@ -11,6 +11,6 @@ interface IJokeDao : IBaseDao<Joke> {
     @Query("SELECT * FROM joke WHERE value = :value")
     suspend fun findByValue(value: String): Joke?
 
-    @Query("SELECT * FROM joke WHERE value LIKE :value")
+    @Query("SELECT * FROM joke WHERE value LIKE ('%' || :value || '%')")
     suspend fun getAllByValue(value: String): List<Joke>?
 }
