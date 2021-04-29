c0e193cabd8e3a243c4cba708c3ffff8bdc622d1
Cameron <cameron.fisher@absa.africa>
2021-04-29 09:24:58 +0200

    Added a transformations.switchmap to resolve the loading issue. - JokeRepository.kt: Added switchmap to fix loading issue.

--- app/src/main/java/com/example/chucknorris/model/repository/JokeRepository.kt
+++ app/src/main/java/com/example/chucknorris/model/repository/JokeRepository.kt
@@ -1,13 +1,23 @@
 package com.example.chucknorris.model.repository
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.chucknorris.model.models.Joke
 import com.example.chucknorris.model.room.ChuckNorrisDatabase
 import com.example.chucknorris.model.service.ChuckNorrisService
 import com.example.chucknorris.utils.DataAccessStrategyUtils
+import com.example.chucknorris.utils.Resource
 
 class JokeRepository(application: Application) {
     private val chuckNorrisService = ChuckNorrisService()
     private val jokeDao = ChuckNorrisDatabase.getDatabase(application).jokeDao()
 
-    fun getJokesBySearch(query: String) = DataAccessStrategyUtils.synchronizedCache(jokeDao, query){ chuckNorrisService.getJokesBySearch(query) }
+    private val queryLiveData by lazy { MutableLiveData<String>()}
+
+    fun getJokesBySearch(query: String): LiveData<Resource<List<Joke>>> {
+        queryLiveData.value = query
+        return Transformations.switchMap(queryLiveData) {DataAccessStrategyUtils.synchronizedCache(jokeDao, it){ chuckNorrisService.getJokesBySearch(it) }}
+    }
 }
